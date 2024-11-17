5029538e2dff317ae9950c93300d5ae31f90159e
sankalp <badonisankalp123@gmail.com>
2024-11-17 22:58:59 +0530

    made utils

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@
 
 import dotenv from "dotenv"
 import express from "express";
-const app = express()
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path:'./env'
 })
 connectDB()
-
+.then(()=>{
+    app.listen(process.env.PORT || 3000)
+})
+.catch((err)=>{
+    console.log('MongoDB connection failed !!',err);
+    
+})
 
 
 /*
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+import { response } from "express"
+
+//Higher order functions i.e. it can take function as parameter. 
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+        
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
+
+const asyncHandler = (reqHandler) => {
+    (req, res, next) => {
+        Promise.resolve(reqHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+export default asyncHandler
