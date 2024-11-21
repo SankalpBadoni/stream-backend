02b5d3f0bf1116417c2d07d0880372d683ababbf
sankalp <badonisankalp123@gmail.com>
2024-11-22 00:31:11 +0530

    updated multer

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -6,3 +6,18 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY, 
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null;
+        const response = await cloudinary.uploader.upload(localFilePath,{
+            resource_type: "auto"
+        })
+        return response
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+        return null
+    }
+}
+
+export {uploadOnCloudinary}
