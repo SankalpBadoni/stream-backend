f1f131c67f4153ca78fd822cb0ac538b4bf8b2a7
sankalp <badonisankalp123@gmail.com>
2025-01-11 00:58:34 +0530

    added channel controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -302,6 +302,76 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     )
 })
 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+    const {username} = req.params
+    if(!username?.trim()){
+        throw new ApiError(400, 'username is missing')
+    }
+
+    //User.find({username}) isme pehle DB se user lenge fir uski id ke basis pe aggregate karenge par itna karne ki zarurat nhi, we can directly use -
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username : username?.toLowerCase()
+            }
+        },
+        {
+            $lookup: {
+                from : "subscriptions", //it has to be "Subscription" but in model everything becomes lowercase and plural
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from : "subscriptions", //it has to be "Subscription" but in model everything becomes lowercase and plural
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields: {
+                subscribersCount : {
+                    $size: "$subscribers"
+                },
+                channelsSubscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: {$in: [req.user?._id, "$subscribers?.subscriber"]},
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                fullname: 1,
+                username: 1,
+                subscribersCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1
+            }
+        }
+    ])
+
+    if(!channel?.length){
+        throw new ApiError(400, "channel does not exist")
+    }
+
+    res
+    .status(200)
+    .json( new ApiResponse(200, channel[0], "user channel fetched successfully"))
+
+})
+
 export {
     registerUser,
     loginUser,
@@ -311,5 +381,7 @@ export {
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    updateUserCoverImage,
+    getUserChannelProfile
 }
